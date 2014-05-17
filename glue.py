f0af3dc8f01cce12606ca4b3748e33622ecd3258
Fabian-Robert Stöter <fabian-robert.stoeter@audiolabs-erlangen.de>
2014-05-18 00:29:16 +0200

    flake8ed

--- glue.py
+++ glue.py
@@ -1,4 +1,3 @@
-#from mir.analyzer import Analyzer
 import numpy as np
 import mir
 import midi
@@ -6,38 +5,34 @@ import time
 import music21
 from functools import partial
 
+
 def myCallback(stream):
     if False:
-        #stream.quantize()
-        copy = stream.chordify() 
-#       copy.show('text')
-        chordList = []; 
+        # stream.quantize()
+        copy = stream.chordify()
+        # copy.show('text')
+        chordList = []
         sChords = copy
         sChordsFlat = sChords.flat
         for myChord in sChordsFlat:
             if "Chord" in myChord.classes:
-                if 2 < len( myChord ) < 6: #and ( myChord.isTriad() ):
+                if 2 < len(myChord) < 6:  # and ( myChord.isTriad() ):
                     chordList.append(myChord.pitchedCommonName)
                     numpyArray = np.array(chordList)
-                    loopIdx = mir.mir.findRepeat( numpyArray );
+                    loopIdx = mir.mir.findRepeat(numpyArray)
                     if loopIdx[0] > -1:
                         print "Found loop at " + str(loopIdx[0])
                         print "Loop score:   " + str(loopIdx[1])
     else:
-        copy = stream #.chordify() 
-#       copy.show('text')
-        noteList = []; 
-        #sChords = copy
-        #sChordsFlat = sChords.flat
+        copy = stream
+        noteList = []
         for myNote in copy:
-            #if "Chord" in myChord.classes:
-            #    if 2 < len( myChord ) < 6: #and ( myChord.isTriad() ):
-                    noteList.append( myNote )
-                    numpyArray = np.array( noteList )
-                    loopIdx = mir.mir.findRepeat( numpyArray );
-                    if loopIdx[0] > -1:
-                        print "Found loop at " + str(loopIdx[0])
-                        print "Loop score:   " + str(loopIdx[1])
+            noteList.append(myNote)
+            numpyArray = np.array(noteList)
+            loopIdx = mir.mir.findRepeat(numpyArray)
+            if loopIdx[0] > -1:
+                print "Found loop at " + str(loopIdx[0])
+                print "Loop score:   " + str(loopIdx[1])
 
 
 stream = music21.stream.Stream()
