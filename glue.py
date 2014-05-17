a5d3a4d1ae5c46cc3df7b6c5539e9cb84da205c1
Thomas <tp_ap@gmx.de>
2014-05-17 23:59:15 +0200

    one finger mode

--- glue.py
+++ glue.py
@@ -35,23 +35,39 @@ def findRepeat( seq ):
         return [loop, loopScore]
 
 def myCallback():
-    copy = stream.chordify() 
-#    copy.show('text')
-    chordList = []; 
-    sChords = copy
-    sChordsFlat = sChords.flat
-    for myChord in sChordsFlat:
-        if "Chord" in myChord.classes:
-           # if 2 < len( myChord ) < 6: #and ( myChord.isTriad() ):
-                chordList.append(myChord.pitchedCommonName)
-                numpyArray = np.array(chordList)
-                loopIdx = findRepeat( numpyArray );
-                if loopIdx[0] > -1:
-                    print "Found loop at " + str(loopIdx[0])
-                    print "Loop score:   " + str(loopIdx[1])
-
+    if False:
+        #stream.quantize()
+        copy = stream.chordify() 
+#       copy.show('text')
+        chordList = []; 
+        sChords = copy
+        sChordsFlat = sChords.flat
+        for myChord in sChordsFlat:
+            if "Chord" in myChord.classes:
+                if 2 < len( myChord ) < 6: #and ( myChord.isTriad() ):
+                    chordList.append(myChord.pitchedCommonName)
+                    numpyArray = np.array(chordList)
+                    loopIdx = findRepeat( numpyArray );
+                    if loopIdx[0] > -1:
+                        print "Found loop at " + str(loopIdx[0])
+                        print "Loop score:   " + str(loopIdx[1])
+    else:
+        copy = stream #.chordify() 
+#       copy.show('text')
+        noteList = []; 
+        #sChords = copy
+        #sChordsFlat = sChords.flat
+        for myNote in copy:
+            #if "Chord" in myChord.classes:
+            #    if 2 < len( myChord ) < 6: #and ( myChord.isTriad() ):
+                    noteList.append( myNote )
+                    numpyArray = np.array( noteList )
+                    loopIdx = findRepeat( numpyArray );
+                    if loopIdx[0] > -1:
+                        print "Found loop at " + str(loopIdx[0])
+                        print "Loop score:   " + str(loopIdx[1])
 
 with midi.reader.Reader(stream) as myReader:
     myReader.register( myCallback )
     while True:
-        time.sleep(0.01)
+        time.sleep(0.001)
