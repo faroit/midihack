c97547922604fc2449b203b338ce7275837e8618
Thomas <tp_ap@gmx.de>
2014-05-18 10:49:46 +0200

    remove consecutive repeated notes

--- glue.py
+++ glue.py
@@ -12,19 +12,20 @@ def myCallback(stream, conn):
     if len(stream) < 1:
         return
 
-    if not conn.song.session_record:
-        conn.song.trigger_session_record()
-
-    if True:
+#    if not conn.song.session_record:
+#        conn.song.trigger_session_record()
+    if False:
         # stream.quantize()
         copy = stream.chordify()
         # copy.show('text')
         chordList = []
         sChords = copy
         sChordsFlat = sChords.flat
+        lastChord = ""
         for myChord in sChordsFlat:
             if "Chord" in myChord.classes:
-                if 2 < len(myChord) < 6:  # and ( myChord.isTriad() ):
+                if 2 <= len(myChord) < 6 and  lastChord != myChord.pitchedCommonName: 
+                    lastChord = myChord.pitchedCommonName;
                     chordList.append(myChord.pitchedCommonName)
                     numpyArray = np.array(chordList)
                     correlation = mir.mir.corrSequence(numpyArray)
@@ -35,14 +36,19 @@ def myCallback(stream, conn):
                         print "Loop score:   " + str(score)
                         time.sleep(60 / 480 * (sChordsFlat[idx].offset -
                                                sChordsFlat[idx-1].offset))
-                        conn.song.trigger_session_record()
+                       # conn.song.trigger_session_record()
                         for i in range(len(stream.notes)):
                             stream.pop(0)
     else:
         copy = stream
         noteList = []
+        lastNote = ""
         for myNote in copy:
-            noteList.append(myNote)
+            if lastNote != myNote:
+                noteList.append(myNote)
+                lastNote = myNote
+            else:
+                continue
             numpyArray = np.array(noteList)
             correlation = mir.mir.corrSequence(numpyArray)
             print correlation
@@ -54,7 +60,7 @@ def myCallback(stream, conn):
                 # print 60 / 480 * (copy.notes[idx].offset - copy[idx-1].offset)
                 time.sleep(60 / 480 * (copy.notes[idx].offset -
                                        copy[idx-1].offset))
-                conn.song.trigger_session_record()
+                # conn.song.trigger_session_record()
                 for i in range(len(stream.notes)):
                     stream.pop(0)
 
@@ -62,7 +68,8 @@ def myCallback(stream, conn):
 stream = music21.stream.Stream()
 
 with midi.reader.Reader(stream) as myReader:
-    conn = live.live.LiveConnection()
+#    conn = live.live.LiveConnection()
+    conn = None
     myReader.register(partial(myCallback, stream, conn))
     while True:
         time.sleep(0.001)
